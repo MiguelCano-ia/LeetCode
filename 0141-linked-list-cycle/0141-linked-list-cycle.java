2102f18b5a2d7834ed0875f9872e3e1a956598e0
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-02-03 18:55:52 -0500

    Time: 0 ms (100.00%), Space: 44.9 MB (5.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,10 +11,6 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-        
         ListNode s = head;
         ListNode f = head;
         
