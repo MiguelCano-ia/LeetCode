dbc49b67414a559ba2681362436b407560f823fa
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-02-03 22:03:38 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (7.87%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,11 +10,7 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-
-        if (head.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
 
