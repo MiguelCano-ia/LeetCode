fd488a724e2a4d5e3002b52b8eb2af0b21e1b262
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-02-08 20:10:58 -0500

    Time: 7 ms (41.46%), Space: 69.1 MB (14.15%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,18 +12,15 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
        ListNode mid = middleNode(head);
        ListNode headSecond = reverseList(mid);
-       ListNode rereverseHead = headSecond;
 
        while (head != null && headSecond != null) {
            if (head.val != headSecond.val) {
-               break;
+               return false;
            }
            head = head.next;
            headSecond = headSecond.next;
        }
-       reverseList(rereverseHead);
-
-       return head == null || headSecond == null;
+       return true;
     }
 
     public ListNode middleNode(ListNode head) {
