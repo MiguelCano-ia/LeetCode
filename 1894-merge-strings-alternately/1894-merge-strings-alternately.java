ae343027ca10fa1747adf5a93c2991541b132252
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-27 19:45:41 -0500

    Time: 7 ms (22.32%), Space: 42.7 MB (17.45%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,23 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         String result = "";
+    
+        int pointer1 = word1.length();
+        int pointer2 = word2.length();
+        
+        while (pointer1 > 0 || pointer2 > 0) {
+            if (pointer1 > 0) {
+                result += word1.charAt(word1.length() - pointer1);
+                pointer1--;
+            }
 
-        int pointer1 = 0;
-        int pointer2 = 0;
-
-        while (pointer1 < word1.length() && pointer2 < word2.length()) {
-            result += word1.charAt(pointer1);
-            result += word2.charAt(pointer2);
-            pointer1++;
-            pointer2++;
+            if (pointer2 > 0) {
+                result += word2.charAt(word2.length() - pointer2);
+                pointer2--;
+            }
         }
-
-        if (pointer1 < word1.length()) {
-            result += word1.substring(pointer1);
-        } else {
-            result += word2.substring(pointer2);
-        }
-
+        
         return result;
     }
 }
