eed70edf2d59c0af2ae1ddc385c95b4106e7f6ac
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-29 17:33:41 -0500

    Time: 2 ms (100%), Space: 44.5 MB (77.31%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,48 +1,36 @@
 class Solution {
     public int romanToInt(String s) {
-        int result = 0;
+        int result = 0, number = 0, prev = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
+            switch (s.charAt(i)) {
+                case 'I' -> number = 1;
+                case 'V' -> number = 5;
+                case 'X' -> number = 10;
+                case 'L' -> number = 50;
+                case 'C' -> number = 100;
+                case 'D' -> number = 500;
+                case 'M' -> number = 1000;
+            }
 
-            if (current == 'I') {
-                result += 1;
-                if (i + 1 < s.length() && s.charAt(i + 1 ) == 'V') {
-                    result += 3;
-                    i++;
-                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'X') {
-                    result += 8;
-                    i++;
+            if (i < s.length() -1) {
+                switch (s.charAt(i + 1)) {
+                    case 'V' -> prev = 5;
+                    case 'X' -> prev = 10;
+                    case 'L' -> prev = 50;
+                    case 'C' -> prev = 100;
+                    case 'D' -> prev = 500;
+                    case 'M' -> prev = 1000;
+                    default -> prev = 0;
                 }
-            } else if (current == 'V') {
-                result += 5;
-            } else if (current == 'X') {
-                result += 10;
-                if (i + 1 < s.length() && s.charAt(i + 1) == 'L') {
-                    result += 30;
-                    i++;
-                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'C') {
-                    result += 80;
-                    i++;
-                }
-            } else if (current == 'L') {
-                result += 50;
-            } else if (current == 'C') {
-                result += 100;
-                if (i + 1 < s.length() && s.charAt(i + 1) == 'D') {
-                    result += 300;
-                    i++;
-                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'M') {
-                    result += 800;
-                    i++;
-                }
-            } else if (current == 'D') {
-                result += 500;
-            } else if (current == 'M') {
-                result += 1000;
+            }
+
+            if (prev > number) {
+                result -= number;
+            } else {
+                result += number;
             }
         }
-
         return result;
     }
 }
