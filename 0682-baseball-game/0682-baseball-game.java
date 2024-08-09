de625bc889aa5fa63ac3cccbcdccab5fb7cb4a42
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-08-09 07:26:47 -0500

    Time: 2 ms (89.07%), Space: 41 MB (99.28%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,28 +1,23 @@
 class Solution {
     public int calPoints(String[] operations) {
         int result = 0;
-        Stack<String> records = new Stack<>();
-
+        Stack<Integer> records = new Stack<>();
 
         for (String operation : operations) {
             switch (operation) {
                 case "C" -> records.pop();
                 case "D" -> {
-                    int peek = Integer.parseInt(records.peek());
-                    int doublePeek = peek * 2;
-                    records.push(String.valueOf(doublePeek));
+                    records.push(records.peek() * 2);
                 }
                 case "+" -> {
-                    int peek = Integer.parseInt(records.elementAt(records.toArray().length - 1));
-                    int beforePeek = Integer.parseInt(records.elementAt(records.toArray().length - 2));
-                    records.push(String.valueOf(peek + beforePeek));
+                    records.push(records.elementAt(records.size() - 1) + records.elementAt(records.size() - 2));
                 }
-                case null, default -> records.push(operation);
+                default -> records.push(Integer.parseInt(operation));
             }
         }
 
-        for (int j = 0; j < records.toArray().length; j++) {
-            result += Integer.parseInt((String) records.toArray()[j]);
+        for (int record : records) {
+            result += record;
         }
 
         return result;
