107c772b157df70579879b537cd8197e9290777a
Miguel Angel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-08-10 17:38:11 -0500

    Time: 1 ms (98.88%), Space: 41.4 MB (64.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() < 2) {
-            return false;
-        }
+        Stack<Character> stack = new Stack<>();
 
-        Stack<Character> valid = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                valid.push('(');
-            } else if (s.charAt(i) == '[') {
-                valid.push('[');
-            } else if (s.charAt(i) == '{') {
-                valid.push('{');
+        for(char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
             } else {
-                if (!valid.empty()) {
-                    if (valid.peek() == '(' && s.charAt(i) == ')') {
-                        valid.pop();
-                    } else if (valid.peek() == '[' && s.charAt(i) == ']') {
-                        valid.pop();
-                    } else if (valid.peek() == '{' && s.charAt(i) == '}') {
-                        valid.pop();
-                    } else {
-                        return false;
-                    }
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+
+                if (c == ')' && top == '(') {
+                    stack.pop();
+                } else if (c == ']' && top == '[') {
+                    stack.pop();
+                } else if (c == '}' && top == '{') {
+                    stack.pop();
                 } else {
                     return false;
                 }
             }
         }
-        return valid.empty();
+
+        return stack.isEmpty();
     }
 }
